3c8c90edca42e413376f374d6ceb3448ecb1d1ce
Paulo Silva <paulo.venones@gmail.com>
2020-07-27 17:12:54 -0300

    initial setup

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png'
+//import ButtonLink from './components/ButtonLink'
+import Button from '../Button'
+import './Menu.css'
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="DevFlix logo"/>
+      </a>
+
+      <Button className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
