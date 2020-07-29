8dcca641f3f173635d3b69d0f57f79a455647054
Paulo Silva <paulo.venones@gmail.com>
2020-07-29 13:24:14 -0300

    change logo and primary color

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por <a href="https://www.linkedin.com/in/paulo-venones-da-silva-9245b5aa/">Paulo Venones</a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
